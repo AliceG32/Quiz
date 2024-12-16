ebda451f30815075f0d89f320efb5aa2cd6a562c
Alisa Galitsyna <galitsyna.ad@phystech.edu>
2024-12-16 22:08:11 +0300

    add QuestionForm

---
+++ forms/QuestionForm.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, RadioField, SelectMultipleField, widgets
+from wtforms.validators import DataRequired
+
+
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
+class QuestionForm(FlaskForm):
+    radioAnswers = RadioField('Выберите верный ответ', choices=[], coerce=int, validators=[DataRequired()])
+    checkAnswers = MultiCheckboxField('Выберите верные ответы', choices=[], coerce=int)
+    submit = SubmitField('Ответить')
+
